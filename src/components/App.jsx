5f69fe8728a46cb2ac6e1e34f7903db4d946ee11
Maryna Vyshnevska <vyshnevska.maryna@gmail.com>
2023-02-14 12:15:21 +0200

    alex

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,20 @@
-
 import ContactCreate from './ContactCreate';
 import ContactList from './ContactList';
 import Section from './Section';
 import Filter from './ContactList/Filter';
 import Header from './Header';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/contact/contact.thunk';
 
 
 export const App = () => {
- 
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+  
   return (
     <>
       <Header title="Phonebook"/>
